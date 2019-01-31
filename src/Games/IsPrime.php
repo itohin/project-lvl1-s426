bab0eb782ab697061e7d806adae84ce0158a359c
itohin <itoxa@MacBook-Pro.local>
2019-01-31 19:28:09 +0300

    isPrime function correction

--- src/Games/IsPrime.php
+++ src/Games/IsPrime.php
@@ -19,6 +19,10 @@ function run()
 
 function isPrime($number)
 {
+    if ($number < 2) {
+        return false;
+    }
+    
     for ($i = 2; $i <= sqrt($number); $i++) {
         if ($number % $i == 0) {
             return false;
