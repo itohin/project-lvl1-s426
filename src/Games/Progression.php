8647d01e366e73b08cafb61f9a39d6b5f97ad07e
itohin <itoxa@MacBook-Pro.local>
2019-01-31 19:23:33 +0300

    Progression refactor

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -6,28 +6,34 @@ use function BrainGames\Kernel\game;
 
 const TASK_OF_GAME = 'What number is missing in the progression?';
 const PROGRESSION_LENGTH = 10;
+const MIN_POSITION = 0;
+const MAX_POSITION = 10;
+const MIN_STEP = 2;
+const MAX_STEP = 7;
 
 function run()
 {
     $getGameData = function () {
-        $firstPosition = rand(0, 9);
-        $interval = rand(2, 7);
+        $firstPosition = rand(MIN_POSITION, MAX_POSITION);
+        $interval = rand(MIN_STEP, MAX_STEP);
 
-        return getProgression($firstPosition, $interval);
+        $progression = getProgression($firstPosition, $interval, PROGRESSION_LENGTH);
+        $correct = $progression[$firstPosition];
+        $progression[$firstPosition] = '..';
+        $question = implode(' ', $progression);
+
+        return [$question, $correct];
     };
     game(TASK_OF_GAME, $getGameData);
 }
 
-function getProgression($firstPosition, $interval)
+function getProgression($firstPosition, $interval, $length)
 {
-    $progression[] = $firstPosition;
+    $progression = [];
 
-    for ($i = 1; $i < PROGRESSION_LENGTH; $i++) {
-        $progression[$i] = $progression[$i - 1] + $interval;
+    for ($i = 0; $i < $length; $i++) {
+        $progression[] = $firstPosition + $interval * $i;
     }
 
-    $correct = $progression[$firstPosition];
-    $progression[$firstPosition] = '..';
-
-    return [implode(' ', $progression), $correct];
+    return $progression;
 }
